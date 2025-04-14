15b8f4b395ff09f95335b650129f17f6d091241a
nbaoquoc05 <baoquoc2132005@gmail.com>
2025-04-14 20:47:55 +0700

    Create Mang2c bai2.c

---
+++ Mang2c bai2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+	// TH1: 
+	int mang1[3][3] = {
+	    {1,2,3},
+	    {4,5,6},
+	    {7,8,9}
+	};
+	printf("Cac phan tu trong mang 1 la : \n");
+	int i,j;
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("%d ",mang1[i][j]);
+		}
+		printf("\n");
+	}
+	
+	// TH2: 
+	
+	int mang2[3][3];
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("Nhap vao cac phan tu cua mang2 : ");
+			scanf("%d ",&mang2[i][j]);
+		}
+	}
+	printf("Cac phan tu cua mang2 la : \n");
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			printf("%d ",mang2[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("Ma tran tong la : \n");
+	return 0;
+}
