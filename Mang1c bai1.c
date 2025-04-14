872dd88cf1219361c6a282ae378add43c12f4f81
nbaoquoc05 <baoquoc2132005@gmail.com>
2025-04-14 20:48:38 +0700

    Create Mang1c bai1.c

---
+++ Mang1c bai1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(){
+	int a[10]={2,4,6,7,9,21,34,56,87,99};
+	printf("Cac phan tu cua mang la : ");
+	int i;
+	for(i=0;i<10;i++){
+		printf("%d ",a[i]);
+	}
+	return 0;
+}
